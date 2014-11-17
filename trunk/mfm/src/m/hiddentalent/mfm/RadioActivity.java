90a84b0a796627388bb6675893696e822e9856f3
filestouch@gmail.com <filestouch@gmail.com@e93056a1-1764-ec9b-4ff5-2a900b31e124>
2014-11-17 10:30:46 +0000

    audio integration

--- trunk/mfm/src/m/hiddentalent/mfm/RadioActivity.java
+++ trunk/mfm/src/m/hiddentalent/mfm/RadioActivity.java
@@ -1,11 +1,51 @@
 package m.hiddentalent.mfm;
 
+import net.droidlabs.audio.ogg.OggStreamPlayer;
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 public class RadioActivity extends Activity {
+	
+	MediaPlayer mMediaPlayer ;
+	OggStreamPlayer player;
+	boolean playing=  false;
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.radio_layout);
+        player = new OggStreamPlayer();
+       
+        ImageView playbutton = (ImageView) findViewById(R.id.imageView3);
+        ImageView stopbutton = (ImageView) findViewById(R.id.imageView4);
+        playbutton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(getApplicationContext(), "play radio", Toast.LENGTH_SHORT).show();
+				
+				try {
+					if (playing){
+					player.playAsync("http://images.wikia.com/saintsrow/images/e/ec/SR2_cemetery_house_Ambients_00574.ogg");
+					}
+					} catch (Exception e) {
+					Toast.makeText(getApplicationContext(), e.toString(), Toast.LENGTH_SHORT).show();	
+				}
+				
+
+			}
+		});
+        stopbutton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				Toast.makeText(getApplicationContext(), "stopping radio", Toast.LENGTH_SHORT).show();
+				player.stop();
+
+			}
+		});
     }
 }
